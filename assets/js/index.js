aaad19a154aee2b49c42766e8afee17c9690afe7
Mohammad Abbasi <mohammad@Mohammads-MacBook-Pro.local>
2024-04-05 09:33:44 -0700

    Button and keyboard listener added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,61 @@
 
-const buttons = document.querySelectorAll(".drum");
-for (let i = 0; i < buttons.length; i++) {
-    const buttonListener = document.querySelectorAll(".drum")[i];
-    buttonListener.addEventListener("click", handleListener);
+const buttons = document.querySelectorAll(".drum").length;
+for (let i = 0; i < buttons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
 
-    function handleListener() {
-        alert("Hello");
+        let buttonInner = this.innerHTML;
+        makeSound(buttonInner);
+    });
+}
+ 
+
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+});
+
+
+
+    function makeSound(key) {
+        
+
+
+        switch (key) {
+            case "w":
+                let audio = new Audio('/Users/mohammad/Desktop/udemy Excersice/drum-kit/assets/sounds/tom-1.mp3');
+                audio.play();
+                break;
+            case "a":
+                let audio1 = new Audio('/Users/mohammad/Desktop/udemy Excersice/drum-kit/assets/sounds/tom-2.mp3');
+                audio1.play();
+                break;
+            case "s":
+                let audio2 = new Audio('/Users/mohammad/Desktop/udemy Excersice/drum-kit/assets/sounds/tom-3.mp3');
+                audio2.play();
+                break;
+            case "d":
+                let audio3 = new Audio('/Users/mohammad/Desktop/udemy Excersice/drum-kit/assets/sounds/tom-4.mp3');
+                audio3.play();
+                break;
+            case "j":
+                let audio4 = new Audio('/Users/mohammad/Desktop/udemy Excersice/drum-kit/assets/sounds/crash.mp3');
+                audio4.play();
+                break;
+            case "k":
+                let audio5 = new Audio('/Users/mohammad/Desktop/udemy Excersice/drum-kit/assets/sounds/kick-bass.mp3');
+                audio5.play();
+                break;
+            case "l":
+                let audio6 = new Audio('/Users/mohammad/Desktop/udemy Excersice/drum-kit/assets/sounds/snare.mp3');
+                audio6.play();
+                break;
+
+            default: console.log(buttonInner);
+        }
+
+
     }
 
-}
 
-
+    // let audio = new Audio('./assets/sounds/tom-1.mp3');
+    // audio.play();
 
